f6b911a4d1c9e6d94899fa40263a1b6525502d66
DAgutierrez <diego.gutierrez684@gmail.com>
2016-02-12 15:18:37 -0300

    adding Linear and Relative Layout

--- app/src/main/java/diego_gutierrez/androidfrontendimplement/MainActivity.java
+++ app/src/main/java/diego_gutierrez/androidfrontendimplement/MainActivity.java
@@ -1,7 +1,10 @@
 package diego_gutierrez.androidfrontendimplement;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final Button btnLayout = (Button) findViewById(R.id.btn_layout);
+
+        btnLayout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LayoutActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
